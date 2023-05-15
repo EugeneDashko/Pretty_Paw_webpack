edc6be97a61f574197c919ff67f2c2ec3acea359
EugeneDashko <eugenedashko@yahoo.com>
2023-05-15 19:45:22 +0300

    19:45 15/05/23

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.html';
 //new modules:
-
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 
 
 
@@ -10,3 +11,11 @@ import './index.html';
 import './index.scss';
 
 //use modules
+
+
+const videoBG = document.querySelector('.video-bg');
+
+videoBG.innerHTML = `
+<source src="video/video.webm" type="video/webm">
+<source src="video/video.mp4" type="video/mp4">
+`;
